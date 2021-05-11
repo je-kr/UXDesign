3d5135409b6e6d7c47b0e7212991014ee4ba52af
Jeremy8988 <jeremy.kritikos@esme.fr>
2021-05-11 15:08:19 +0200

    Created MenuMedecin

---
+++ app/src/main/java/com/example/ux_design/MenuMedecin.java
@@ -0,0 +1,14 @@
+package com.example.ux_design;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuMedecin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_medecin);
+    }
+}
