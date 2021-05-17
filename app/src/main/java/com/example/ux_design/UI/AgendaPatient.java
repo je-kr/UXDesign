53efa25c8ed4925c8a0a9f0fcf65724c673c73f5
Jeremy8988 <jeremy.kritikos@esme.fr>
2021-05-17 16:08:40 +0200

    Ajout Menu Patient

---
+++ app/src/main/java/com/example/ux_design/UI/AgendaPatient.java
@@ -0,0 +1,18 @@
+package com.example.ux_design.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ux_design.R;
+
+public class AgendaPatient extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agenda_patient);
+
+
+    }
+}
